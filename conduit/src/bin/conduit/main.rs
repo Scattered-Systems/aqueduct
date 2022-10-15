c6bfbcd4faeb64000f7df74fa76bcdc267607676
Joe McCain III <jo3mccain@scattered-systems.com>
2022-10-15 01:28:45 +0000

    Update

---
+++ conduit/src/bin/conduit/main.rs
@@ -0,0 +1,13 @@
+/*
+    Appellation: conduit <binary>
+    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
+    Description:
+        A high-preformant proxy created for developing secure, cloud-native applications
+*/
+
+#[tokio::main]
+async fn main() -> scsys::core::BoxResult {
+    
+    Ok(())
+}
+
