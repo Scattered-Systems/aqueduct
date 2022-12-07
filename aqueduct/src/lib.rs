228167b51e6f52b6203c8d55fcc2173680185aa3
Joe McCain III <jo3mccain@icloud.com>
2022-12-07 00:18:53 +0000

    Update

--- aqueduct/src/lib.rs
+++ aqueduct/src/lib.rs
@@ -1,6 +1,14 @@
 /*
-    Appellation: conduit-sdk <library>
-    Contributors: FL03 <jo3mccain@icloud.com> (https://github.com/FL03)
+    Appellation: aqueduct <library>
+    Contrib: FL03 <jo3mccain@icloud.com> (https://github.com/FL03)
     Description:
-        Aqueduct implements the tweakble sleeves recently formalized by researchers at IOHK giving quantum resilience to non-quantum elliptic-curve cryptocraphic schemes
+        Aqueduct is a hybrid encryption protocol combining aspects of homomorphic encryption with elliptic-curve cryptography leveraging the modularity theorem to
+        naturally bridge the two distinctly different approaches.
 */
+#[cfg(feature = "sleeves")]
+pub use aqueduct_sleeves as sleeves;
+
+pub mod prelude {
+    #[cfg(feature = "sleeves")]
+    pub use crate::sleeves::*;
+}
---
+++ sleeves/src/lib.rs
@@ -0,0 +1,6 @@
+/*
+    Appellation: aqueduct-sleeves <library>
+    Contrib: FL03 <jo3mccain@icloud.com> (https://github.com/FL03)
+    Description:
+        Aqueduct implements the tweakble sleeves recently formalized by researchers at IOHK giving quantum resilience to non-quantum elliptic-curve cryptocraphic schemes
+*/
